1752aa8f226537a4acd13f941ecd4037895c0bed
akovtun-extenda <57349964+akovtun-extenda@users.noreply.github.com>
2020-08-21 12:34:46 +0300

    feat: Create Scheduler module (#1)
    * feat: Create Scheduler module
    
    * fix: add new line; change values
    
    * add some changes
    
    * Update the README
    
    Co-authored-by: Anastasiia Kovtun <nastia@Anastasiias-MacBook-Pro-2.local>

---
+++ variables.tf
@@ -0,0 +1,22 @@
+variable create_job {
+  type        = bool
+  description = "Specify true if you want to create a job"
+  default     = true
+}
+
+variable project_id {
+  type        = string
+  description = "Project ID where the jobs will be created"
+}
+
+ variable region {
+   type    = string
+   description = "Region where the scheduler job resides. If it is not provided, Terraform will use the provider default"
+   default = "europe-west1"
+ }
+
+variable scheduled_jobs {
+  type        = list(map(string))
+  description = "The list of the jobs to be created"
+  default     = []
+}
