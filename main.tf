84c6ffa49eef29798427010b2edaf930be7982ba
Aleksandr Skoriy <al.skoriy@gmail.com>
2021-06-02 15:10:48 +0300

    feat: Add min_backoff_duration and max_backoff_duration parameters

--- main.tf
+++ main.tf
@@ -24,8 +24,10 @@ resource "google_cloud_scheduler_job" "job" {
   dynamic "retry_config" {
     for_each = (lookup(var.scheduled_jobs[count.index], "retry_count", "") != "") ? [var.scheduled_jobs[count.index].retry_count] : []
     content {
-      retry_count        = lookup(var.scheduled_jobs[count.index], "retry_count", "")
-      max_retry_duration = lookup(var.scheduled_jobs[count.index], "max_retry_duration", "")
+      retry_count          = lookup(var.scheduled_jobs[count.index], "retry_count", "")
+      max_retry_duration   = lookup(var.scheduled_jobs[count.index], "max_retry_duration", "")
+      min_backoff_duration = lookup(var.scheduled_jobs[count.index], "min_backoff_duration", "")
+      max_backoff_duration = lookup(var.scheduled_jobs[count.index], "max_backoff_duration", "")
     }
   }
 
