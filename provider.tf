01c2a8034d534a6fbf9d3a10e3439eaf2440d3d6
Anastasiia Kovtun <anastasiia.kovtun@extendaretail.com>
2022-02-17 10:44:28 +0100

    chore: Pin provider versions

--- provider.tf
+++ provider.tf
@@ -4,6 +4,10 @@ terraform {
   required_providers {
     google = {
       source = "hashicorp/google"
+      version = "4.0.0"
+    }
+    google-beta = {
+      source = "hashicorp/google-beta"
       version = "~> 4.0.0"
     }
   }
